07699058122362876fbc0edf145ca19018e0f6d5
alecgilchrist <alecgilchrist@gmail.com>
2018-05-28 09:08:40 -0500

    TODO 6 finished

--- projects/billypedia/index.js
+++ projects/billypedia/index.js
@@ -2,15 +2,56 @@
 
 $(document).ready(function() {
     $.getJSON('data.json', function (data) {
+
+      $('#section-bio').css('color', 'rgba(51, 12, 24, 0.78)');
+      $('body').css("background-color", 'rgba(129, 126, 149, 0.76)');
+      $('header').css("background-color", 'rgba(172, 146, 186, 0.78)');
+      $('nav').css("background-color", 'rgba(24, 24, 24, 0.36)')
+      $('#section-quotes').css('color', 'rgba(51, 12, 24, 0.78)')
+      $('#section-praise').css('color', 'rgba(51, 12, 24, 0.78)')
+
+      const topRated = data.discography.topRated;
+      const topRatedListItems = _.map(topRated, function(recording) {
+            return $('<li>').addClass('list-top-rated')
+                .append($('<div>').addClass('top-rated-title').text(recording.title));
+        });
+        $('#list-top-rated').append(topRatedListItems);
+
+        const recordings = data.discography.recordings;
+        const recordingsListItems = _.map(recordings, recording => {
+          return $('<li>').addClass('recording')
+          // .append($('<div>').attr('id', 'image-container-recordings').addClass('image-container').append("<img id='recording-image' src='recording.art'>"))
+          .append($('<div>').addClass('title').text(recording.title))
+          .append($('<div>').addClass('artist').text(recording.artist))
+          .append($('<div>').addClass('release').text(recording.release))
+          .append($('<div>').addClass('year').text(recording.year))
+        })
+        const $sectionRecordings = $('<section>').attr('id', 'section-recordings').addClass('recordings');
+        const $headerRecordings = $('<header>').addClass('header-recordings').text('General Recordings');
+        const $listRecordings = $('<ul>').attr('id', 'list-recordings').append(recordingsListItems);
+        const $recordingImages = $('<div>').attr('id', 'recording-image').addClass('image').append($('<div>').attr('id', 'image-container-recordings').addClass('image-container').append("<img id='recording-image' src='images/album/eastern-rebellion.jpg'>"));
+
+        $sectionRecordings
+            .append($headerRecordings)
+            .append($recordingImages)
+            .append($listRecordings)
+            .appendTo('#sidebar');
+
+console.log(data.discography)
+
+            // <div id="image-container-recording" class="image-container">
+            //     <img id="recording-image" src="images/album/eastern-rebellion.jpg" class="image">
+            // </div>
+
         // YOUR CODE BELOW HERE //
 
         // uncomment this to inspect all available data; delete when done //
 
         // EXAMPLE: Looping over top rated recordings; replace with your code //
-        // let topRated = data.discography.topRated;
-        // _.forEach(topRated, function(recording) {
-        //     console.log(recording);
-        // });
+
+        _.forEach(topRated, function(recording) {
+            console.log(recording);
+        });
 
 
         // YOUR CODE ABOVE HERE //
